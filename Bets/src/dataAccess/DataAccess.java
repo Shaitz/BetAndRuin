9998ee0d2244479a651e73d9191c0817cc9faeab
Unai Heras <Unai Heras@192.168.1.132>
2021-05-13 17:19:24 +0200

    Events are created with predefined questions

--- Bets/src/dataAccess/DataAccess.java
+++ Bets/src/dataAccess/DataAccess.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Vector;
